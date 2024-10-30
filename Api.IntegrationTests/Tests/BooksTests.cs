4312676fcbf0586982ab08cf2d30978d76bc4ed4
thomasbrowne0 <144432350+thomasbrowne0@users.noreply.github.com>
2024-10-30 15:28:42 +0100

    testing testing

--- Api.IntegrationTests/Tests/BooksTests.cs
+++ Api.IntegrationTests/Tests/BooksTests.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using System.Net.Http.Json;
 using System.Text.Json;
 using Generated;
@@ -20,5 +21,50 @@ public class BooksTests : ApiTestBase
         Assert.Equivalent(result.Genre, dto.Genre);
         Assert.Equivalent(result.Title, dto.Title);
         Assert.NotEqual(0, result.Id);
+    }
+
+    [Fact]
+    public async Task CreateBook_FailsWhenAuthorAndGenreEmpty()
+    {
+        var dto = new CreateBookDto()
+        {
+            Author = "",
+            Genre = "",
+            Title = "Valid Book"
+        };
+        try
+        {
+            var response = await new LibraryClient(Client).PostAsync(dto);
+            Assert.False(response.StatusCode == 200);
+        }
+        catch (ApiException ex)
+        {
+            Assert.Equal(400, ex.StatusCode);
+        }
+    }
+
+    [Fact]
+    public async Task CantBeLoanedWhenAlreadyLoaned()
+    {
+        var dto = new LoanBookDto()
+        {
+            BookId = 1,
+            UserId = 1
+        }; 
+        var result = (await new LibraryClient(Client).LoanAsync(dto)).Result;
+        var Seconddto = new LoanBookDto()
+        {
+            BookId = result.Id,
+            UserId = 2
+        };
+        try
+        {
+            var response = await new LibraryClient(Client).LoanAsync(Seconddto);
+            Assert.False(response.StatusCode == 200);
+        }
+        catch (ApiException ex)
+        {
+            Assert.Equal(500, ex.StatusCode);
+        }
     }
 }
